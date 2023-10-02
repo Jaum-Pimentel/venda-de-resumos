1cef372ed13e606ba8b89f050e2b76bf14cf1ac1
Jaum-Pimentel <145376721+Jaum-Pimentel@users.noreply.github.com>
2023-10-02 09:28:44 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarResumos() {
+    const resumosContainer = document.getElementById('resumosContainer');
+    resumosContainer.style.display = 'flex';
+  
+    const header = document.querySelector('.header');
+    header.style.display = 'none';
+  }
